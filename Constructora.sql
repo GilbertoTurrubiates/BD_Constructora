f4329f87a4b9ba0ae9b3bbf62526416304a01e3d
Gilberto Turrubiates <159360311+GilbertoTurrubiates@users.noreply.github.com>
2025-02-22 18:13:10 -0600

    Add files via upload

--- Constructora.sql
+++ Constructora.sql
@@ -3,9 +3,10 @@ CREATE TABLE usuarios (
     nombre VARCHAR(50) NOT NULL,
     apellidoP VARCHAR(50) NOT NULL,
     apellidoM VARCHAR(50) NOT NULL,
-    correoPersonal VARCHAR(100) UNIQUE NOT NULL,
+    correoPersonal VARCHAR(100) NOT NULL,
+    correoTrabajo VARCHAR(100) UNIQUE NOT NULL,
     contraseña VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NULL,
+    telefono VARCHAR(10) NULL,
     rol ENUM('Supervisor', 'Proveedor') NOT NULL,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     activo BOOLEAN DEFAULT TRUE
